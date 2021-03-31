368106c7cc0f98ae9a9cc5d60fcc0704f1a8e165
AhadKhan98 <ahad_zai@berea.edu>
2021-03-31 19:21:44 +0500

    add countries to selectpicker using api

--- react-frontend/src/components/CountryPicker/CountryPicker.jsx
+++ react-frontend/src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import {fetchCountries} from '../../api';
 
 const CountryPicker = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountriesAsync = async () => {
+            const fetchedCountries = await fetchCountries();
+            setCountries(fetchedCountries);
+        }
+        fetchCountriesAsync();
+    }, [])
+
+
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+                {countries ? countries.map(country => <option key={country} value={country}>{country}</option>) : null}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
