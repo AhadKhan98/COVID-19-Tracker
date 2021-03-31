121b60727d7861bf9fb98b49359f0864fa8a6864
AhadKhan98 <ahad_zai@berea.edu>
2021-03-31 17:52:04 +0500

    add styling to app

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import { Card, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Covid App</h1>
+        <Card />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
